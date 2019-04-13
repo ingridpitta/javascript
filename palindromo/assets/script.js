4cad4d5ceebccda37bbd84fb298985058b214c5c
ingridpitta <ingridpitta056@gmail.com>
2019-04-13 14:04:14 -0300

    palindromo__final

--- palindromo/assets/script.js
+++ palindromo/assets/script.js
@@ -1,26 +1,13 @@
 function palindromo(){
-    var palavra = document.getElementById("input").value.toLowerCase();
+    var palavra = document.getElementById("input").value.toLowerCase().replace(/ /g, "");
     var resposta = document.getElementById("resposta");
-
-
-    while(palavra.indexOf(" ") != -1){
-        palavra = palavra.replace(" ", "");
-        return palavra;
-    }
-    
     for(var i = 0; i < palavra.length/2; i++){
-
         if(palavra[i] !== palavra[palavra.length -1 -i]){
             resposta.innerText = "Não é =("
             return
         }
-
         else{
             resposta.innerText = "Sim! =D"
-            return
         }
     }
-
-   
-
 }
