0750ad1153607fba4fc2e512f5594d335a0d064a
ingridpitta <ingridpitta056@gmail.com>
2019-04-26 15:47:38 -0300

    blue_ok

--- teste_blue/assets/script.js
+++ teste_blue/assets/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function (event) {
-
+    //Mudar classe (css) de acordo com click do usuário
     let allForms = document.querySelectorAll('.conteudo');
 
     allForms.forEach(function(form){
@@ -24,8 +24,9 @@ window.addEventListener('DOMContentLoaded', function (event) {
     let resultado = document.querySelector("#btnResultado");
 
     resultado.addEventListener("click", function gerarResultado(){
-        let listaResultado = ["./assets/img/blue_ata.jpg", "./assets/img/blue_domadora_de_brancas.jpg", "./assets/img/blue_dona_da_porra_toda.gif","./assets/img/blue_forcada.jpeg", "./assets/img/blue_domadora.jpg", "./assets/img/blue_meiga.jpeg", "./assets/img/blue_minha_filhaaaaa.jpg", "./assets/img/blue_nhaaam.jpg", "./assets/img/blue_princesa.jpg", "./assets/img/blue_really.jpg", "./assets/img/blue_sem_paciencia.jpg", "./assets/img/blue_sem_tempo_irmao.jpg"];
-        
+        //Resultados Possíveis
+        let listaResultado = ["./assets/img/blue_ata.jpg", "./assets/img/blue_domadora_de_brancas.jpg", "./assets/img/blue_dona_da_porra_toda.gif","./assets/img/blue_forcada.jpeg", "./assets/img/blue_julgadora.jpg", "./assets/img/blue_meiga.jpeg", "./assets/img/blue_minha_filhaaaaa.jpg", "./assets/img/blue_nhaaam.jpg", "./assets/img/blue_princesa.jpg", "./assets/img/blue_really.jpg", "./assets/img/blue_sem_paciencia.jpg", "./assets/img/blue_sem_tempo_irmao.jpg"];
+        //Pegar valor das seleções
         let selecionado = document.querySelectorAll('.habilitado');
         let value1 = selecionado[0]
         let value2 = selecionado[1]
@@ -35,44 +36,50 @@ window.addEventListener('DOMContentLoaded', function (event) {
         let opcao2 = value2.id;
         let opcao3 = value3.id;
 
-
-        let escolhaNegros = ["#childishGambino" ,"#hER" , "#cardiB"] ;
-        let escolhaBrancos =  ["#duaLipa" , "#ladyGaga" , "#mileyCyrus"];
-        let escolhaBlue = ["#arte" , "#bitcoin" , "#viagem"];
-        let escolhaOutro =  ["#carro", "#casa" , "#festa"] ;
-        let escolhaBey = ["#ohYeah" , "#teyTey" , "#aham"] ;
-        let escolhaJay =  ["#rsrs" , "#omg" , "#naoCreio"];
-
-
-        console.log(opcao1,opcao2,opcao3);
-        switch(opcao1,opcao2,opcao3){
-
-            case ([escolhaNegros].includes(opcao1) && [escolhaBlue].includes(opcao2) && [escolhaBey].includes(opcao3)):
-                blueResultado.src = listaResultado[0];
+        //Respostas Primeira Pergunta
+        let escolhaNegros = ["childishGambino" ,"hER" , "cardiB"] ;
+        let escolhaBrancos =  ["duaLipa" , "ladyGaga" , "mileyCyrus"];
+        //Respostas Segunda Pergunta
+        let escolhaBlue = ["arte" , "bitcoin" , "viagem"];
+        let escolhaOutro =  ["carro", "casa" , "festa"] ;
+        //Respostas Terceira Pergunta
+        let escolhaBey = ["ohYeah" , "teyTey" , "aham"] ;
+        let escolhaJay =  ["rsrs" , "omg" , "naoCreio"];
+        //Respostas Selecionadas
+        let resultado = [opcao1,opcao2,opcao3];
+        //Condições possíveis
+        for(let i=0; i < resultado.length; i++){
+            if(escolhaNegros.includes(resultado[i]) && escolhaBlue.includes(resultado[i+1]) && escolhaBey.includes(resultado[i+2])){
+                blueResultado.src = listaResultado[8];
             break;
-
-            case ([escolhaBrancos].includes(opcao1) && [escolhaOutro].includes(opcao2) && [escolhaJay].includes(opcao3)):
-                blueResultado.src = listaResultado[1];
+            }
+            else if(escolhaBrancos.includes(resultado[i]) && escolhaOutro.includes(resultado[i+1]) && escolhaJay.includes(resultado[i+2])){
+                blueResultado.src = listaResultado[6];
             break;
-
-            case ([escolhaNegros].includes(opcao1) && [escolhaBlue].includes(opcao2) && [escolhaJay].includes(opcao3)):
+            }
+            else if(escolhaNegros.includes(resultado[i]) && escolhaBlue.includes(resultado[i+1]) && escolhaJay.includes(resultado[i+2])){
                 blueResultado.src = listaResultado[2];
             break;
-
-            case ([escolhaBrancos].includes(opcao1) && [escolhaBlue].includes(opcao2) && [escolhaBey].includes(opcao3)):
+            }
+            else if(escolhaBrancos.includes(resultado[i]) && escolhaBlue.includes(resultado[i+1]) && escolhaBey.includes(resultado[i+2])){
                 blueResultado.src = listaResultado[3];
             break;
-
-            case ([escolhaNegros].includes(opcao1) && [escolhaOutro].includes(opcao2) && [escolhaJay].includes(opcao3)):
-                blueResultado.src = listaResultado[4];
+            }
+            else if(escolhaNegros.includes(resultado[i]) && escolhaOutro.includes(resultado[i+1]) && escolhaJay.includes(resultado[i+2])){
+                blueResultado.src = listaResultado[9];
             break;
-
-            case ([escolhaBrancos].includes(opcao1) && [escolhaOutro].includes(opcao2) && [escolhaBey].includes(opcao3)):
+            }
+            else if(escolhaBrancos.includes(resultado[i]) && escolhaOutro.includes(resultado[i+1]) && escolhaBey.includes(resultado[i+2])){
+                blueResultado.src = listaResultado[11];
+            break;
+            }
+            else if(escolhaNegros.includes(resultado[i]) && escolhaOutro.includes(resultado[i+1]) && escolhaBey.includes(resultado[i+2])){
+                blueResultado.src = listaResultado[10];
+            break;
+            }
+            else{
                 blueResultado.src = listaResultado[5];
-            break;
-            default:
-                alert("Por favor selecione uma imagem para cada pergunta!");
-            break;
+            }
         };
     });     
 });
